8b87d7bd65cbb8eebc9ebaf3d5cfb1f0131b5ca9
MikeKhay <khajmike@gmail.com>
2020-10-01 16:50:31 +0300

    nhb

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ sendMessageForm.send.addEventListener("click",function(){
 function setInfo(name, phone, message){
     let postInfo = new XMLHttpRequest();
 
-    postInfo.open("GET", `https://api.telegram.org/bot1371367465:AAH9ohtMAoaXVJ6PMAP61kc-mxDe2eNv574/sendMessage?text=name: ${name}\n phone: ${phone} \n message: ${message}&chat_id=-438067797`, false)
+    postInfo.open("GET", `https://api.telegram.org/bot1371367465:AAH9ohtMAoaXVJ6PMAP61kc-mxDe2eNv574/sendMessage?
+    text=name:${name}\n phone: ${phone} \n message: ${message}&chat_id=-438067797`, false);
 
     postInfo.send();
 }
